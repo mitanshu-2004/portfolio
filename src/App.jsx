270b88e1d6c1234a2d343af8e6db5adc9317fc29
mitanshu <mitanshug2004@gmail.com>
2025-08-31 22:37:36 +0530

    loggin

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react"
+
 import SocialBar from "./components/SocialBar"
 import About from "./components/About"
 import Experience from "./components/Experience"
@@ -6,6 +8,12 @@ import Skills from "./components/Skills"
 import Footer from "./components/Footer"
 
 function App() {
+  useEffect(() => {
+    const apiUrl = import.meta.env.VITE_API_URL  // <- use env variable
+    fetch(`${apiUrl}/log`, { method: "POST" })
+      .catch((err) => console.error("Log error:", err))
+  }, [])
+
   return (
     <div className="bg-gray-950 text-white">
       <SocialBar />
