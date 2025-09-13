89511c2a9f57ee31c170ffa28e294ea109477cf9
mitanshu <mitanshug2004@gmail.com>
2025-09-13 22:11:17 +0530

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,37 @@ import './globals.css';
 import Analytics from './components/Analytics';
 
 export const metadata = {
-  title: 'My Portfolio',
-  description: 'A portfolio of my work.',
+  title: 'Mitanshu Goel',
+  description:
+    'Engineering student specializing in robotics, AI, and full-stack development. ',
   icons: {
     icon: '/portfolio.png',
   },
   openGraph: {
-    images: '/back.png',
+    title: 'Mitanshu Goel | Robotics, AI & Software Engineer',
+    description:
+      'Engineering student specializing in robotics, AI, and full-stack development. ',
+    url: 'https://mitanshu.me',
+    siteName: 'Mitanshu Goel Portfolio',
+    images: [
+      {
+        url: '/back.png',
+        width: 1200,
+        height: 630,
+        alt: 'Mitanshu Goel Portfolio',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Mitanshu Goel | Robotics, AI & Software Engineer',
+    description:
+      'Engineering student specializing in robotics, AI, and full-stack development.',
+    images: ['/back.png'],
   },
 };
+
 
 export default function RootLayout({
   children,
